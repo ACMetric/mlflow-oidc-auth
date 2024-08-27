efaf988da12017246e72f08840173606b241ffbd
Maximilian Hofmann <max@acmetric.org>
2024-08-27 14:56:42 +0000

    add psycopg2

--- scripts/run-dev-server.sh
+++ scripts/run-dev-server.sh
@@ -13,6 +13,7 @@ python_preconfigure() {
     source venv/bin/activate
     python3 -m pip install --upgrade pip
     python3 -m pip install build setuptools
+    python3 -m pip install psycopg2-binary
     python3 -m pip install -e .
   fi
 }
